8bd7fdb2f2586fef4f82e054756c6d2920c497a1
KristionM <kam319@miami.edu>
2018-08-27 00:37:14 -0400

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(2000, 2000);
+  background (204);
+}
+
+function draw() {
+  // put drawing code here
+  //body
+  ellipse(300,300,550,300)
+  //mouth
+    ellipse(300,340,350,75)
+    //left eye
+      ellipse(150,160,60,60)
+      //eye
+        ellipse(450,160,60,60)
+}
+
+//js stands for java script , set ups draw once while draw puts up 60 frames per second
